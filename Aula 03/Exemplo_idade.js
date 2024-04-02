8d160bf46bcd2ddb45982c91c42ef2a5907fc681
Gabriel Sousa <90335814+GaBRiel4115@users.noreply.github.com>
2024-04-01 21:59:17 -0300

    Add files via upload

---
+++ Aula 03/Exemplo_idade.js
@@ -0,0 +1,10 @@
+// Exemplo 2 - Verificação de idade 
+
+let idade = parseInt(prompt("Digite sua idade: "));
+
+// Idade >= 18
+if (idade >= 18) {
+  alert ("Você é maior de idade!");
+} else {
+  alert("Você é menor de idade");
+}
---
+++ Aula 03/Exemplo_soma.js
@@ -0,0 +1,7 @@
+//Exemplo 1 -Realizando a soma de dois números
+let num1 = parseFloat(prompt("Digite o primero número"));
+let num2 = parseFloat(prompt("Digite o segundo número"));
+
+let soma = num1 + num2;
+
+alert("soma é: " + soma);
