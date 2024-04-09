5731c6a3cd6138113adcf7ef5dc90dbd1a421a0a
Gabriel Sousa <90335814+GaBRiel4115@users.noreply.github.com>
2024-04-08 21:55:28 -0300

    Add files via upload

---
+++ Aula 03/Ex04.js
@@ -0,0 +1,16 @@
+let nota1 = parseFloat(prompt("Digite a primeira nota."))
+let nota2 = parseFloat(prompt("Digite a segunda nota."))
+let nota3 = parseFloat(prompt("Digite a terceira nota."))
+
+let soma = nota1+ nota2 + nota3
+let media = soma/3
+alert ("A média é: " + media)
+
+    if (media >= 7){
+        alert ("Parabéns! Você foi aprovado.")
+    }else
+        if( media >=5){
+           alert("Aluno em recuperação.") 
+        }else{
+            alert ("Reprovado.")
+    }
