cba32ea747ab362851b246d9fa09b989ceb02b57
Gabriel Sousa <90335814+GaBRiel4115@users.noreply.github.com>
2024-05-07 14:49:14 -0300

    Delete Aula 03/Ex_NumeroPrimoIncompleto.js

--- Aula 03/Ex_NumeroPrimoIncompleto.js
+++
@@ -1,7 +0,0 @@
-// Determinar se um número é primo.
-//Definição: Um número primo é um numero natural divisivel apenas por ele mesmo e por 1.
-
-let num = parseFloat(prompt("Digite um número"));
-    if (num % ){
-        alert ("O numero é NÃO é primo.");
-    }
